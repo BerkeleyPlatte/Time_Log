a0edd7cd9262506ebf45b46d129e5a27f5a9c6ed
berkeley platte <berkeleyplatte989@gmail.com>
2019-10-29 10:52:46 -0500

    registration done

--- timelog4app/urls.py
+++ timelog4app/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 from timelog4app.views import *
 
 app_name = "timelog4app"
@@ -6,4 +6,7 @@ app_name = "timelog4app"
 urlpatterns = [
     url(r'^$', home, name='home'),
     url(r'^activities$', activity_list, name='activities'),
+    url(r'accounts/', include('django.contrib.auth.urls')),
+    url(r'^logout/$', logout_user, name='logout'),
+    url(r'^register/$', register_user, name='register'),
 ]
--- timelog4app/views/__init__.py
+++ timelog4app/views/__init__.py
@@ -1,2 +1,4 @@
 from .activities.list import activity_list
 from .home import home
+from .auth.register import register_user
+from .auth.logout import logout_user
