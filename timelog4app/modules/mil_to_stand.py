272cdf51c58e058aecb769a45be327bfc3ee90e6
berkeley platte <berkeleyplatte989@gmail.com>
2019-11-05 15:50:50 -0600

    mvp yay

--- timelog4app/modules/mil_to_stand.py
+++ timelog4app/modules/mil_to_stand.py
@@ -9,8 +9,7 @@ def mil_to_stand(mil):
             stand = mil
         else:
 
-            stand = datetime.datetime.strptime(
-                mil[:5], '%H:%M').strftime('%I:%M %p')
+            stand = datetime.datetime.strptime(mil[:5], '%H:%M').strftime('%I:%M %p')
 
         return stand
     else:
