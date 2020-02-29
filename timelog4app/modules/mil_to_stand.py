03e73292b59d477fa7f39c3a69b17989084d4364
berkeley platte <berkeleyplatte989@gmail.com>
2020-02-29 16:41:32 -0600

    one

--- timelog4app/modules/mil_to_stand.py
+++ timelog4app/modules/mil_to_stand.py
@@ -1,16 +1,7 @@
 import datetime
 
-
 def mil_to_stand(mil):
-
     if mil is not None:
-        sliced = mil[:2]
-        if mil[6:] is "AM" or mil[6:] is "PM" or mil[:2] is "AM" or mil[:2] is 'PM':
-            stand = mil
-        else:
-
-            stand = datetime.datetime.strptime(mil[:5], '%H:%M').strftime('%I:%M %p')
-
-        return stand
+        return datetime.datetime.strptime(mil[:5], '%H:%M').strftime('%I:%M %p')
     else:
         return mil
