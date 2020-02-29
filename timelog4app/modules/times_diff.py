f31ad0997b66f72278732384966cbce382cb442e
berkeley platte <berkeleyplatte989@gmail.com>
2020-02-29 16:42:27 -0600

    two

--- timelog4app/modules/times_diff.py
+++ timelog4app/modules/times_diff.py
@@ -1,14 +1,14 @@
 def times_diff(start, stop):
-    if stop is None:
-        start_mins = 0
-        stop_mins = 0
-        diff_in_mins = stop_mins - start_mins
-    elif int(stop[:2]) < int(start[:2]):
-        mins_before_midnight = (1440 - (int(start[:2]) * 60) + int(start[3:5]))
-        mins_after_midnight = (int(stop[:2]) * 60) + int(stop[3:5])
+    start_hours = int(start[:2])
+    start_mins = int(start[3:5])
+    stop_hours = int(stop[:2])
+    stop_mins = int(stop[3:5])
+    if stop_hours < start_hours:
+        mins_before_midnight = (1440 - (start_hours * 60)) - start_mins
+        mins_after_midnight = (stop_hours * 60) + stop_mins
         diff_in_mins = mins_before_midnight + mins_after_midnight
     else:
-        start_mins = (int(start[:2]) * 60) + int(start[3:5])
-        stop_mins = (int(stop[:2]) * 60) + int(stop[3:5])
-        diff_in_mins = stop_mins - start_mins
+        total_start_mins = (start_hours * 60) + start_mins
+        total_stop_mins = (stop_hours * 60) + stop_mins
+        diff_in_mins = total_stop_mins - total_start_mins
     return diff_in_mins
